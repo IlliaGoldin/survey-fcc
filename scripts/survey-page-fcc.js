e74e5ca08be8bd0aee5c85f3260d5cedb08bdb7e
IGoldin <squl2squl@hotmail.com>
2018-12-20 18:57:09 -0700

    Removed junk JS

--- scripts/survey-page-fcc.js
+++ scripts/survey-page-fcc.js
@@ -2,12 +2,3 @@ $(document).ready(function () {
     jcf.replaceAll();
 });
 
-function submitConfirm() {
-    alert(
-        "Hi!\n" +
-        "If you liked this page and want me to build one for you HERE is my contact information.\n" +
-        "If you'd like to see more of my work visit LINK.\n" +
-        "If you'd like to learn more about me and my professional background visit LINKEDIN PROFILE.\n" +
-        "Thanks for stopping by and have a nice day!"
-    );
-};
